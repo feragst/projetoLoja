6d41b1a9a5af398bf9a999304be81edb862a85e9
Fernando Agostini <fernandoagostini30@gmail.com>
2025-04-15 20:31:36 -0300

    create cclass liente

---
+++ projetoFinal/src/models/Cliente.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package models;
+
+/**
+ *
+ * @author fernando.agostini
+ */
+public class Cliente {
+    String nome;
+    String cpf;
+    int Telefone;
+      public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public int getTelefone() {
+        return Telefone;
+    }
+
+    public void setTelefone(int Telefone) {
+        this.Telefone = Telefone;
+    }
+    
+}
