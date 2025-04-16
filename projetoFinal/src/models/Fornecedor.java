aacb7fa871ba0df2f735880420efaad7757e1a9c
Fernando Agostini <fernandoagostini30@gmail.com>
2025-04-15 22:00:13 -0300

    fornecedor

--- projetoFinal/src/models/Fornecedor.java
+++ projetoFinal/src/models/Fornecedor.java
@@ -9,5 +9,59 @@ package models;
  * @author fernando.agostini
  */
 public class Fornecedor {
+    private int idFornecedor;
+    private String nome;
+    private String cnpj;
+    private String telefone;
+    private String endereco;
+
+    public int getIdFornecedor() {
+        return idFornecedor;
+    }
+
+    public void setIdFornecedor(int idFornecedor) {
+        this.idFornecedor = idFornecedor;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public Fornecedor(int idFornecedor, String nome, String cnpj, String telefone, String endereco) {
+        this.idFornecedor = idFornecedor;
+        this.nome = nome;
+        this.cnpj = cnpj;
+        this.telefone = telefone;
+        this.endereco = endereco;
+    }
+    
     
 }
